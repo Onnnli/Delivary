3308623bf1cba57b58a32e828b59b6e55d74901e
onnnli <souslayk@gmail.com>
2021-06-19 14:20:59 +0300

    Added create product

--- src/controller/InitAppController.java
+++ src/controller/InitAppController.java
@@ -26,30 +26,42 @@ public class InitAppController {
                 ClientController clientController = new ClientController(inputReader);
                 Client client = clientController.createClient();
                 System.out.println(client.name);
+
                 InitAppController.createTask();
             }
             case "2" -> {
                 CourierController courierController = new CourierController(inputReader);
                 Courier courier = courierController.createCourier();
                 System.out.println(courier.name);
+
                 InitAppController.createTask();
             }
             case "3" -> {
                 OrderController orderController = new OrderController(inputReader);
                 Order order = orderController.createOrder();
                 System.out.println(order.name);
+
                 InitAppController.createTask();
             }
             case "4" -> {
                 DB.executeQuery("SELECT * FROM Clients");
+
                 InitAppController.createTask();
             }
             case "5" -> {
                 DB.executeQuery("SELECT * FROM Couriers");
+
                 InitAppController.createTask();
             }
             case "6" -> {
                 DB.executeQuery("SELECT * FROM Orders");
+
+                InitAppController.createTask();
+            }
+            case "7" -> {
+                ProductController productController = new ProductController(inputReader);
+                productController.createProduct();
+
                 InitAppController.createTask();
             }
             default -> System.out.println("Выход");
@@ -63,6 +75,7 @@ public class InitAppController {
         System.out.println("4: Вывести имена всех клиентов");
         System.out.println("5: Вывести имена всех курьеров");
         System.out.println("6: Вывести имена всех заказов");
+        System.out.println("7: Создать продукт продукт");
 
         return inputReader.getString();
     }
